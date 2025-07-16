6bedce5c9fa60edacd593da6700877e25a2a8fe5
nVITTOadrianto <nvittoa@live.com>
2025-07-16 15:30:11 +0700

    Menambahkan favicon ke website

--- resources/views/admin/layouts/app.blade.php
+++ resources/views/admin/layouts/app.blade.php
@@ -4,6 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <title>Dashboard - AplikasiPI</title>
+    <link rel="shortcut icon" href="{{ asset('logo_pi.png') }}" type="image/png">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.css">
